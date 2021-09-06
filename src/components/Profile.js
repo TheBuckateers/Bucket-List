bd979728199aabe15bdbe6e1e94c9e6d72134899
Willem Jacobs <wamj58@gmail.com>
2021-09-05 19:40:07 -0500

    auth0 added

---
+++ src/components/Profile.js
@@ -0,0 +1,60 @@
+import React, { useEffect, useState } from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+
+const Profile = () => {
+  const { user, isAuthenticated, getAccessTokenSilently } = useAuth0();
+  const [userMetadata, setUserMetadata] = useState(null);
+
+  const showUserInfo = () => {
+    console.log("User Info from Auth0: ", user);
+  };
+
+  useEffect(() => {
+    const getUserMetadata = async () => {
+      const domain = "dev-bqwezv2c.us.auth0.com";
+
+      try {
+        const accessToken = await getAccessTokenSilently({
+          audience: `https://${domain}/api/v2/`,
+          scope: "read:current_user",
+        });
+
+        const userDetailsByIdUrl = `https://${domain}/api/v2/users/${user.sub}`;
+
+        const metadataResponse = await fetch(userDetailsByIdUrl, {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        });
+
+        const { user_metadata } = await metadataResponse.json();
+
+        setUserMetadata(user_metadata);
+      } catch (e) {
+        console.log(e.message);
+      }
+    };
+
+    getUserMetadata();
+  }, [getAccessTokenSilently, user?.sub]);
+
+  return (
+    isAuthenticated && (
+      <div className="m-4">
+        <img src={user.picture} alt={user.name} />
+        <h2 onClick={showUserInfo} style={{ cursor: "pointer" }}>
+          {user.name}
+        </h2>
+        <p>{user.email}</p>
+        <h3>User Metadata</h3>
+        {userMetadata ? (
+          <pre>{JSON.stringify(userMetadata, null, 2)}</pre>
+        ) : (
+          "No user metadata defined"
+        )}
+      </div>
+    )
+  );
+};
+
+export default Profile;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -10,11 +11,19 @@ import Layout from "./components/UI/Layout";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <Layout>
-        <App />
-      </Layout>
-    </Router>
+    <Auth0Provider
+      domain="dev-bqwezv2c.us.auth0.com"
+      clientId="ZN4K3OEBG8UhBcW7S4rX5MHE9ouuYPb4"
+      redirectUri={window.location.origin}
+      audience="https://dev-bqwezv2c.us.auth0.com/api/v2/"
+      scope="read:current_user update:current_user_metadata"
+    >
+      <Router>
+        <Layout>
+          <App />
+        </Layout>
+      </Router>
+    </Auth0Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
